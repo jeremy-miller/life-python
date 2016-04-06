02276fb90ab623a27863b22fee40b39b7d09434f
Jeremy Miller <jgmiller88@gmail.com>
2016-04-05 22:58:27 -0700

    switch to using numpy

--- life/main.py
+++ life/main.py
@@ -1,77 +1,70 @@
 import logging
+import numpy
 from sys import exit
 from life.logger import LoggerClass
 
 
 class LifeClass(object):
-  def __init__(self):
-    pass
+  def __init__(self, size):
+    self._size = size
+    self._grid = self.generate_grid()
 
   def main(self):
-    height = 5
-    width = 5
-    grid = [[False for x in range(height)] for x in range(width)]
-    grid = self.blinker(grid)
-    self.run(grid, height, width)
+    self.blinker()
+    self.run()
 
+  def generate_grid(self):
+    return numpy.zeros((self._size, self._size), dtype=numpy.int)
 
-  def blinker(self, grid):
-    grid[2][1] = True
-    grid[2][2] = True
-    grid[2][3] = True
-    return grid
+  def blinker(self):
+    self._grid[2][1] = 1
+    self._grid[2][2] = 1
+    self._grid[2][3] = 1
 
-  def run(self, grid, height, width):
-    new_grid = self.rules(grid, height, width)
-    self.run(new_grid, height, width)
+  def run(self):
+    self._grid = self.run_iteration()
+    self.run()
 
-  def rules(self, grid, height, width):
-    new_grid = [[False for x in range(height)] for x in range(width)]
-    for row in range(len(grid)):
-      for column in range(len(grid[row])):
+  def run_iteration(self):
+    new_grid = self.generate_grid()
+    for row in range(self._grid.shape[0]):
+      for column in range(self._grid.shape[0]):
         live_neighbors = 0
 
-        if row-1 >= 0 and column-1 >= 0 and grid[row-1][column-1]:
+        if row-1 >= 0 and column-1 >= 0 and self._grid[row-1][column-1]:
           live_neighbors += 1
-        if row-1 >= 0 and column >= 0 and grid[row-1][column]:
+        if row-1 >= 0 and column >= 0 and self._grid[row-1][column]:
           live_neighbors += 1
-        if row-1 >= 0 and column+1 < width and grid[row-1][column+1]:
+        if row-1 >= 0 and column+1 < self._grid.shape[0] and self._grid[row-1][column+1]:
           live_neighbors += 1
-        if column-1 >= 0 and grid[row][column-1]:
+        if column-1 >= 0 and self._grid[row][column-1]:
           live_neighbors += 1
-        if column+1 < width and grid[row][column+1]:
+        if column+1 < self._grid.shape[0] and self._grid[row][column+1]:
           live_neighbors += 1
-        if row+1 < height and column-1 >= 0 and grid[row+1][column-1]:
+        if row+1 < self._grid.shape[0] and column-1 >= 0 and self._grid[row+1][column-1]:
           live_neighbors += 1
-        if row+1 < height and column >= 0 and grid[row+1][column]:
+        if row+1 < self._grid.shape[0] and column >= 0 and self._grid[row+1][column]:
           live_neighbors += 1
-        if row+1 < height and column+1 < width and grid[row+1][column+1]:
+        if row+1 < self._grid.shape[0] and column+1 < self._grid.shape[0] and self._grid[row+1][column+1]:
           live_neighbors += 1
 
-        if grid[row][column] and live_neighbors < 2:
-          new_grid[row][column] = False
-        elif grid[row][column] and (live_neighbors == 2 or live_neighbors == 3):
-          new_grid[row][column] = True
-        elif grid[row][column] and live_neighbors > 3:
-          new_grid[row][column] = False
-        elif not grid[row][column] and live_neighbors == 3:
-          new_grid[row][column] = True
+        if self._grid[row][column] and live_neighbors < 2:
+          new_grid[row][column] = 0
+        elif self._grid[row][column] and (live_neighbors == 2 or live_neighbors == 3):
+          new_grid[row][column] = 1
+        elif self._grid[row][column] and live_neighbors > 3:
+          new_grid[row][column] = 0
+        elif not self._grid[row][column] and live_neighbors == 3:
+          new_grid[row][column] = 1
         else:
-          new_grid[row][column] = False
-    return new_grid
-
-
-# Any live cell with fewer than two live neighbours dies, as if caused by under-population.
-# Any live cell with two or three live neighbours lives on to the next generation.
-# Any live cell with more than three live neighbours dies, as if by over-population.
-# Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-
+          new_grid[row][column] = 0
+    self._grid = new_grid
 
 if __name__ == '__main__':
   try:
     LoggerClass().create_logger()
     logging.info("Starting Conway's Game of Life...")
-    LifeClass().main()
+    LifeClass().main(5)
   except KeyboardInterrupt:
     logging.info('Exiting...')
     exit()
