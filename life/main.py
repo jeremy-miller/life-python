34fe63f9b2cb91c602d6ce5fbea6d5db70494d40
Jeremy Miller <jgmiller88@gmail.com>
2016-04-07 22:41:34 -0700

    use numpy sum to calculate neighbors, ndindex to iterate over array

--- life/main.py
+++ life/main.py
@@ -7,14 +7,11 @@ from life.logger import LoggerClass
 class LifeClass(object):
   def __init__(self, size):
     self._size = size
-    self._grid = self.generate_grid()
+    self._grid = numpy.zeros((self._size, self._size), dtype=numpy.int)
 
   def main(self):
     self.blinker()
     self.run()
-
-  def generate_grid(self):
-    return numpy.zeros((self._size, self._size), dtype=numpy.int)
 
   def blinker(self):
     self._grid[2][1] = 1
@@ -22,49 +19,32 @@ class LifeClass(object):
     self._grid[2][3] = 1
 
   def run(self):
-    self._grid = self.run_iteration()
+    self.run_iteration()
     self.run()
 
+  def sum_neighbors(self, row, column):
+    # use numpy's array slicing to "slice" out 9 cells (current cell and its 8 neighbors) and sum their 'living' values
+    # then remove the current cell's value from the final sum since we're only finding living neighbors
+    # also make sure we take boundary conditions into account
+    return numpy.sum(self._grid[max(row - 1, 0):min(row + 2, self._size), max(column - 1, 0):min(column + 2, self._size)]) - self._grid[row][column]
+
   def run_iteration(self):
-    new_grid = self.generate_grid()
-    for row in range(self._grid.shape[0]):
-      for column in range(self._grid.shape[0]):
-        live_neighbors = 0
-
-        if row-1 >= 0 and column-1 >= 0 and self._grid[row-1][column-1]:
-          live_neighbors += 1
-        if row-1 >= 0 and column >= 0 and self._grid[row-1][column]:
-          live_neighbors += 1
-        if row-1 >= 0 and column+1 < self._grid.shape[0] and self._grid[row-1][column+1]:
-          live_neighbors += 1
-        if column-1 >= 0 and self._grid[row][column-1]:
-          live_neighbors += 1
-        if column+1 < self._grid.shape[0] and self._grid[row][column+1]:
-          live_neighbors += 1
-        if row+1 < self._grid.shape[0] and column-1 >= 0 and self._grid[row+1][column-1]:
-          live_neighbors += 1
-        if row+1 < self._grid.shape[0] and column >= 0 and self._grid[row+1][column]:
-          live_neighbors += 1
-        if row+1 < self._grid.shape[0] and column+1 < self._grid.shape[0] and self._grid[row+1][column+1]:
-          live_neighbors += 1
-
-        if self._grid[row][column] and live_neighbors < 2:
+    new_grid = numpy.copy(self._grid)  # rule 2
+    for row, column in numpy.ndindex(self._grid.shape):  # ndindex returns every (row, column) pair in the array
+      live_neighbors = self.sum_neighbors(row, column)
+      if self._grid[row][column]:  # rule 1 and 3
+        if live_neighbors < 2 or live_neighbors > 3:
           new_grid[row][column] = 0
-        elif self._grid[row][column] and (live_neighbors == 2 or live_neighbors == 3):
-          new_grid[row][column] = 1
-        elif self._grid[row][column] and live_neighbors > 3:
-          new_grid[row][column] = 0
-        elif not self._grid[row][column] and live_neighbors == 3:
-          new_grid[row][column] = 1
-        else:
-          new_grid[row][column] = 0
+      elif live_neighbors == 3:  # rule 4
+        new_grid[row][column] = 1
     self._grid = new_grid
+
 
 if __name__ == '__main__':
   try:
     LoggerClass().create_logger()
     logging.info("Starting Conway's Game of Life...")
-    LifeClass().main(5)
+    LifeClass(5).main()
   except KeyboardInterrupt:
     logging.info('Exiting...')
     exit()
