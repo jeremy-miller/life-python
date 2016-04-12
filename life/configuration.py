ce94fda989eecf6e103f2e323290b674e9868d76
Jeremy Miller <jgmiller88@gmail.com>
2016-04-11 21:45:47 -0700

    add docstrings

--- life/configuration.py
+++ life/configuration.py
@@ -1,21 +1,44 @@
+"""This module reads, parses, validates, and returns the Life configuration file."""
+
 import logging
 from os.path import dirname, join, realpath
 from yaml import load
 
 
-class ConfigurationClass(object):
+class ConfigurationClass(object):  # pylint: disable=too-few-public-methods
+  """This class reads, parses, validates, and returns the Life configuration file.
+
+  Attributes:
+    _CONFIG_FILENAME (string): The name of the configuration YAML file located in the local folder.
+  """
   _CONFIG_FILENAME = 'config.yml'
 
   def __init__(self):
     self._set_configuration()
 
   def _set_configuration(self):
+    """This method sets the class variable 'self._configuration.'"""
     self._configuration = self._parse_yaml()
     self._validate_configuration()
 
   def _parse_yaml(self):
-    filepath = join(dirname(realpath(__file__)), self._CONFIG_FILENAME)
+    """This method parses the Life configuration file.
+
+    This method loads and parses the YAML configuration file from the local folder.
+    The name of the configuration file is set in the class variable '_CONFIG_FILENAME'
+
+    Returns:
+      A dictionary containing the configuration file keys and values.  For example:
+
+      {
+        'rows': 40,
+        'columns': 40,
+        'starting_configuration': 'gosper_glider_gun',
+        'delay': 1
+      }
+    """
     logging.info('Loading and parsing %s', self._CONFIG_FILENAME)
+    filepath = join(dirname(realpath(__file__)), self._CONFIG_FILENAME)
     logging.debug('%s filepath: %s', self._CONFIG_FILENAME, filepath)
     with open(filepath, 'r') as f:
       parsed_yaml = load(f)
@@ -23,13 +46,23 @@ class ConfigurationClass(object):
     return parsed_yaml
 
   def _validate_configuration(self):
+    """This method validates the configuration values.
+
+    This method provides validation of the values within the class variable self._configuration.
+    It validates that the expected keys are in the configuration dictionary, as well as
+    that each value is the expected type.  It also verifies that certain settings have a minimum value.
+    This method does not validate that the 'starting_configuration' setting contains one of the
+    expected strings, since this validation is done within the 'patterns.py' file.
+
+    Raises:
+      AssertionError: A validation error occurred.
+    """
     logging.debug('Validating configuration')
     assert 'rows' in self._configuration, 'Missing "rows" setting'
     assert isinstance(self._configuration['rows'], int), '"rows" setting must be an integer'
     assert 'columns' in self._configuration, 'Missing "columns" setting'
     assert isinstance(self._configuration['columns'], int), '"columns" setting must be an integer'
     assert 'starting_configuration' in self._configuration, 'Missing "starting_configuration" setting'
-    # checking whether the 'starting_configuration' setting is valid will happen in patterns.py
     assert 'delay' in self._configuration, 'Missing "delay" setting'
     assert isinstance(self._configuration['delay'], int), '"delay" setting must be an integer'
     if self._configuration['rows'] < 40:
