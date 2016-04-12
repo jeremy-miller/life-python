8cb7e7e44b098fc88e49c5768193b7112bb5de19
Jeremy Miller <jgmiller88@gmail.com>
2016-04-11 22:41:56 -0700

    add docstrings for main.py

--- life/main.py
+++ life/main.py
@@ -1,13 +1,20 @@
+#!/usr/bin/python
+
+"""This module runs the Life game."""
+
 import logging
 from life.logger import LoggerClass
 from life.game import GameClass
 
 
 class MainClass(object):
+  """This class runs the Life game."""
   def __init__(self):
+    """This method initializes the Life game."""
     self._game = GameClass()
 
   def run(self):
+    """This method begins running the Life game."""
     self._game.run()
 
 
