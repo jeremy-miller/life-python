923ca5a7083d9833e74a6c5e312c54ff9c2b3209
Jeremy Miller <jgmiller88@gmail.com>
2016-04-11 22:38:50 -0700

    add docstrings for logger.py

--- life/logger.py
+++ life/logger.py
@@ -1,10 +1,21 @@
+"""This module creates a logger for formatted console logging."""
+
 import logging
 from sys import stdout
 
 
 class LoggerClass(object):
+  """This class creates a logger for formatted console logging."""
   @staticmethod
   def create_logger(log_level=logging.INFO):
+    """This function creates a logger for formatted console logging.
+
+    Args:
+      log_level (int): The level at which to log.  Default is INFO.
+
+    Returns:
+      The configured logger.
+    """
     logger = logging.getLogger('')
     logger.setLevel(log_level)
     log_formatter = logging.Formatter("%(asctime)s %(message)s", datefmt='[%d/%b/%Y %H:%M:%S]')
