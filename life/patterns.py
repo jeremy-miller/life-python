fb2b97c38f65db1f1d815533ebfa8a6119d6e902
Jeremy Miller <jgmiller88@gmail.com>
2016-04-14 07:48:38 -0700

    simplify starting configuration setup

--- life/patterns.py
+++ life/patterns.py
@@ -16,191 +16,250 @@ class PatternsClass(object):  # pylint: disable=R0903
     """This method instantiates the grid and defines the starting configuration functions.
 
     The grid is initialized to a Numpy array of all zeroes, since that is the default 'dead' state.
+    The config_functions dictionary contains all the valid starting_configuration names, along
+    with the associated indices which must be marked as 'living' at the beginning of the game.
 
     Args:
       configuration (dict): The configuration information parsed from the local configuration file.
     """
     self.grid = numpy.zeros((configuration['rows'], configuration['columns']), dtype=numpy.int)
     self._config_functions = {
-      'blinker': self._set_blinker,
-      'glider': self._set_glider,
-      'r-pentomino': self._set_r_pentomino,
-      'toad': self._set_toad,
-      'pulsar': self._set_pulsar,
-      'pentadecathlon': self._set_pentadecathlon,
-      'lightweight_spaceship': self._set_lightweight_spaceship,
-      'gosper_glider_gun': self._set_gosper_glider_gun
+      'blinker': self._get_blinker(),
+      'glider': self._get_glider(),
+      'r-pentomino': self._get_r_pentomino(),
+      'toad': self._get_toad(),
+      'pulsar': self._get_pulsar(),
+      'pentadecathlon': self._get_pentadecathlon(),
+      'lightweight_spaceship': self._get_lightweight_spaceship(),
+      'gosper_glider_gun': self._get_gosper_glider_gun()
     }
 
   def set_configured_grid(self, starting_configuration):
     """This method sets the starting 'live' cells in the 'dead' grid.
 
-    If the provided 'starting_configuration' variable is not a valid option,
-    the game is ended.  If the 'starting_configuration' variable is a valid option,
-    the corresponding function is executed to initialize the grid with the correct
-    starting configuration.
+    If the 'starting_configuration' variable is a valid option, the corresponding
+    indices are set to 'living' in the grid.  If the provided 'starting_configuration'
+    variable is not a valid option, the game is ended.
 
     Args:
       starting_configuration (str): The starting configuration string.
     """
+    logging.debug('Setting starting configuration for %s', starting_configuration)
     if starting_configuration in self._config_functions:
-      self._config_functions[starting_configuration]()
+      for index in self._config_functions[starting_configuration]:
+        self.grid[index[0]][index[1]] = 1
     else:
       logging.error('Invalid starting configuration: %s', starting_configuration)
       exit(1)
 
-  def _set_blinker(self):
-    """This method sets the 'blinker' starting configuration"""
-    logging.debug('Setting initial grid configuration: blinker')
-    self.grid[2][1] = 1
-    self.grid[2][2] = 1
-    self.grid[2][3] = 1
+  @staticmethod
+  def _get_blinker():
+    """This function gets the 'blinker' starting configuration
 
-  def _set_glider(self):
-    """This method sets the 'glider' starting configuration"""
-    logging.debug('Setting initial grid configuration: glider')
-    self.grid[1][2] = 1
-    self.grid[2][3] = 1
-    self.grid[3][1] = 1
-    self.grid[3][2] = 1
-    self.grid[3][3] = 1
+    Returns:
+      The list of indices which should be set to 'living' (i.e. 1) for this
+      starting configuration.
+    """
+    return [
+      (2, 1),
+      (2, 2),
+      (2, 3)
+    ]
 
-  def _set_r_pentomino(self):
-    """This method sets the 'r-pentomino' starting configuration"""
-    logging.debug('Setting initial grid configuration: r-pentomino')
-    self.grid[1][2] = 1
-    self.grid[1][3] = 1
-    self.grid[2][1] = 1
-    self.grid[2][2] = 1
-    self.grid[3][2] = 1
+  @staticmethod
+  def _get_glider():
+    """This function gets the 'glider' starting configuration
 
-  def _set_toad(self):
-    """This method sets the 'toad' starting configuration"""
-    logging.debug('Setting initial grid configuration: toad')
-    self.grid[1][2] = 1
-    self.grid[1][3] = 1
-    self.grid[1][4] = 1
-    self.grid[2][1] = 1
-    self.grid[2][2] = 1
-    self.grid[2][3] = 1
+    Returns:
+      The list of indices which should be set to 'living' (i.e. 1) for this
+      starting configuration.
+    """
+    return [
+      (1, 2),
+      (2, 3),
+      (3, 1),
+      (3, 2),
+      (3, 3)
+    ]
 
-  def _set_pulsar(self):
-    """This method sets the 'pulsar' starting configuration"""
-    logging.debug('Setting initial grid configuration: pulsar')
-    self.grid[1][3] = 1
-    self.grid[1][4] = 1
-    self.grid[1][5] = 1
-    self.grid[1][9] = 1
-    self.grid[1][10] = 1
-    self.grid[1][11] = 1
-    self.grid[3][1] = 1
-    self.grid[3][6] = 1
-    self.grid[3][8] = 1
-    self.grid[3][13] = 1
-    self.grid[4][1] = 1
-    self.grid[4][6] = 1
-    self.grid[4][8] = 1
-    self.grid[4][13] = 1
-    self.grid[5][1] = 1
-    self.grid[5][6] = 1
-    self.grid[5][8] = 1
-    self.grid[5][13] = 1
-    self.grid[6][3] = 1
-    self.grid[6][4] = 1
-    self.grid[6][5] = 1
-    self.grid[6][9] = 1
-    self.grid[6][10] = 1
-    self.grid[6][11] = 1
-    self.grid[8][3] = 1
-    self.grid[8][4] = 1
-    self.grid[8][5] = 1
-    self.grid[8][9] = 1
-    self.grid[8][10] = 1
-    self.grid[8][11] = 1
-    self.grid[9][1] = 1
-    self.grid[9][6] = 1
-    self.grid[9][8] = 1
-    self.grid[9][13] = 1
-    self.grid[10][1] = 1
-    self.grid[10][6] = 1
-    self.grid[10][8] = 1
-    self.grid[10][13] = 1
-    self.grid[11][1] = 1
-    self.grid[11][6] = 1
-    self.grid[11][8] = 1
-    self.grid[11][13] = 1
-    self.grid[13][3] = 1
-    self.grid[13][4] = 1
-    self.grid[13][5] = 1
-    self.grid[13][9] = 1
-    self.grid[13][10] = 1
-    self.grid[13][11] = 1
+  @staticmethod
+  def _get_r_pentomino():
+    """This function gets the 'r-pentomino' starting configuration
 
-  def _set_pentadecathlon(self):
-    """This method sets the 'pentadecathlon' starting configuration"""
-    logging.debug('Setting initial grid configuration: pentadecathlon')
-    self.grid[4][5] = 1
-    self.grid[5][5] = 1
-    self.grid[6][4] = 1
-    self.grid[6][6] = 1
-    self.grid[7][5] = 1
-    self.grid[8][5] = 1
-    self.grid[9][5] = 1
-    self.grid[10][5] = 1
-    self.grid[11][4] = 1
-    self.grid[11][6] = 1
-    self.grid[12][5] = 1
-    self.grid[13][5] = 1
+    Returns:
+      The list of indices which should be set to 'living' (i.e. 1) for this
+      starting configuration.
+    """
+    return [
+      (1, 2),
+      (1, 3),
+      (2, 1),
+      (2, 2),
+      (3, 2)
+    ]
 
-  def _set_lightweight_spaceship(self):
-    """This method sets the 'lightweight spaceship' starting configuration"""
-    logging.debug('Setting initial grid configuration: lightweight_spaceship')
-    self.grid[1][1] = 1
-    self.grid[1][4] = 1
-    self.grid[2][5] = 1
-    self.grid[3][1] = 1
-    self.grid[3][5] = 1
-    self.grid[4][2] = 1
-    self.grid[4][3] = 1
-    self.grid[4][4] = 1
-    self.grid[4][5] = 1
+  @staticmethod
+  def _get_toad():
+    """This function gets the 'toad' starting configuration
 
-  def _set_gosper_glider_gun(self):
-    """This method sets the 'Gosper glider gun' starting configuration"""
-    logging.debug('Setting initial grid configuration: gosper_glider_gun')
-    self.grid[1][25] = 1
-    self.grid[2][23] = 1
-    self.grid[2][25] = 1
-    self.grid[3][13] = 1
-    self.grid[3][14] = 1
-    self.grid[3][21] = 1
-    self.grid[3][22] = 1
-    self.grid[3][35] = 1
-    self.grid[3][36] = 1
-    self.grid[4][12] = 1
-    self.grid[4][16] = 1
-    self.grid[4][21] = 1
-    self.grid[4][22] = 1
-    self.grid[4][35] = 1
-    self.grid[4][36] = 1
-    self.grid[5][1] = 1
-    self.grid[5][2] = 1
-    self.grid[5][11] = 1
-    self.grid[5][17] = 1
-    self.grid[5][21] = 1
-    self.grid[5][22] = 1
-    self.grid[6][1] = 1
-    self.grid[6][2] = 1
-    self.grid[6][11] = 1
-    self.grid[6][15] = 1
-    self.grid[6][17] = 1
-    self.grid[6][18] = 1
-    self.grid[6][23] = 1
-    self.grid[6][25] = 1
-    self.grid[7][11] = 1
-    self.grid[7][17] = 1
-    self.grid[7][25] = 1
-    self.grid[8][12] = 1
-    self.grid[8][16] = 1
-    self.grid[9][13] = 1
-    self.grid[9][14] = 1
+    Returns:
+      The list of indices which should be set to 'living' (i.e. 1) for this
+      starting configuration.
+    """
+    return [
+      (1, 2),
+      (1, 3),
+      (1, 4),
+      (2, 1),
+      (2, 2),
+      (2, 3)
+    ]
+
+  @staticmethod
+  def _get_pulsar():
+    """This function gets the 'pulsar' starting configuration
+
+    Returns:
+      The list of indices which should be set to 'living' (i.e. 1) for this
+      starting configuration.
+    """
+    return [
+      (1, 3),
+      (1, 4),
+      (1, 5),
+      (1, 9),
+      (1, 10),
+      (1, 11),
+      (3, 1),
+      (3, 6),
+      (3, 8),
+      (3, 13),
+      (4, 1),
+      (4, 6),
+      (4, 8),
+      (4, 13),
+      (5, 1),
+      (5, 6),
+      (5, 8),
+      (5, 13),
+      (6, 3),
+      (6, 4),
+      (6, 5),
+      (6, 9),
+      (6, 10),
+      (6, 11),
+      (8, 3),
+      (8, 4),
+      (8, 5),
+      (8, 9),
+      (8, 10),
+      (8, 11),
+      (9, 1),
+      (9, 6),
+      (9, 8),
+      (9, 13),
+      (10, 1),
+      (10, 6),
+      (10, 8),
+      (10, 13),
+      (11, 1),
+      (11, 6),
+      (11, 8),
+      (11, 13),
+      (13, 3),
+      (13, 4),
+      (13, 5),
+      (13, 9),
+      (13, 10),
+      (13, 11)
+    ]
+
+  @staticmethod
+  def _get_pentadecathlon():
+    """This function gets the 'pentadecathlon' starting configuration
+
+    Returns:
+      The list of indices which should be set to 'living' (i.e. 1) for this
+      starting configuration.
+    """
+    return [
+      (4, 5),
+      (5, 5),
+      (6, 4),
+      (6, 6),
+      (7, 5),
+      (8, 5),
+      (9, 5),
+      (10, 5),
+      (11, 4),
+      (11, 6),
+      (12, 5),
+      (13, 5)
+    ]
+
+  @staticmethod
+  def _get_lightweight_spaceship():
+    """This function gets the 'lightweight spaceship' starting configuration
+
+    Returns:
+      The list of indices which should be set to 'living' (i.e. 1) for this
+      starting configuration.
+    """
+    return [
+      (1, 1),
+      (1, 4),
+      (2, 5),
+      (3, 1),
+      (3, 5),
+      (4, 2),
+      (4, 3),
+      (4, 4),
+      (4, 5)
+    ]
+
+  @staticmethod
+  def _get_gosper_glider_gun():
+    """This function gets the 'gosper glider gun' starting configuration
+
+    Returns:
+      The list of indices which should be set to 'living' (i.e. 1) for this
+      starting configuration.
+    """
+    return [
+      (1, 25),
+      (2, 23),
+      (2, 25),
+      (3, 13),
+      (3, 14),
+      (3, 21),
+      (3, 22),
+      (3, 35),
+      (3, 36),
+      (4, 12),
+      (4, 16),
+      (4, 21),
+      (4, 22),
+      (4, 35),
+      (4, 36),
+      (5, 1),
+      (5, 2),
+      (5, 11),
+      (5, 17),
+      (5, 21),
+      (5, 22),
+      (6, 1),
+      (6, 2),
+      (6, 11),
+      (6, 15),
+      (6, 17),
+      (6, 18),
+      (6, 23),
+      (6, 25),
+      (7, 11),
+      (7, 17),
+      (7, 25),
+      (8, 12),
+      (8, 16),
+      (9, 13),
+      (9, 14)
+    ]
